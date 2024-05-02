b4ef40cfb33e521e42fa978fedfd6b40d9a46821
Adrian Adamiak <adrian@adamiak.net>
2024-05-02 14:26:25 +0000

    Temorarily inline all similarity calculations to AddressAnalyzer

--- packages/discovery/src/discovery/analysis/AddressAnalyzer.ts
+++ packages/discovery/src/discovery/analysis/AddressAnalyzer.ts
@@ -4,7 +4,9 @@ import {
   ContractValue,
   UpgradeabilityParameters,
 } from '@l2beat/discovery-types'
+import { existsSync, readdirSync, readFileSync } from 'fs'
 import { isEqual } from 'lodash'
+import { join } from 'path'
 
 import { flattenStartingFrom } from '../../flatten/flattenStartingFrom'
 import {
@@ -48,6 +50,20 @@ export interface AnalyzedEOA {
   address: EthereumAddress
 }
 
+const TEMPLATES_PATH = join('discovery', '_templates')
+const TEMPLATE_SHAPE_FOLDER = 'shape'
+
+interface HashedChunks {
+  content: string
+  length: number
+}
+
+interface HashedFileContent {
+  path: string
+  hashChunks: HashedChunks[]
+  content: string
+}
+
 export class AddressAnalyzer {
   constructor(
     private readonly provider: DiscoveryProvider,
@@ -84,9 +100,11 @@ export class AddressAnalyzer {
     )
 
     const flattened = flattenMainSource(sources)
-    console.log(flattened)
+
 
     logger.logName(sources.name)
+    console.log(sources.name)
+    findSimilarTemplates(flattened)
 
     const { results, values, errors } = await this.handlerExecutor.execute(
       address,
@@ -200,9 +218,12 @@ function getRelevantValues(
 }
 
 function flattenMainSource(sources: ContractSources): string {
-  const source = sources.sources[0]
+  // Skip the proxy and get the first source
+  const source =
+    sources.sources.length === 1 ? sources.sources[0] : sources.sources[1]
+
   if (source === undefined) {
-    throw Error('asdf')
+    throw Error('No sources found')
   }
 
   const input: FileContent[] = Object.entries(source.source.files)
@@ -219,4 +240,206 @@ function flattenMainSource(sources: ContractSources): string {
 
   const output = flattenStartingFrom(source.name, parsedFileManager)
   return output
+}
+
+function iterateFoldersRecursively(
+  path: string,
+  callback: (path: string) => void,
+): void {
+  const folders = readdirSync(path, { withFileTypes: true }).filter((x) =>
+    x.isDirectory(),
+  )
+
+  if (existsSync(join(path, 'template.jsonc'))) {
+    callback(path)
+  }
+
+  for (const folder of folders) {
+    iterateFoldersRecursively(`${path}/${folder.name}`, callback)
+  }
+}
+
+function findSimilarTemplates(flatSource: string): void {
+  const content = removeComments(flatSource)
+  const sourceHashed: HashedFileContent = {
+    path: '',
+    hashChunks: buildSimilarityHashmap(content),
+    content,
+  }
+  iterateFoldersRecursively(TEMPLATES_PATH, (path) => {
+    console.log(path)
+
+    const shapeFolder = join(path, TEMPLATE_SHAPE_FOLDER)
+    if (existsSync(shapeFolder)) {
+      const files = readdirSync(shapeFolder, {
+        withFileTypes: true,
+      }).filter((x) => x.isFile() && x.name.endsWith('.sol'))
+      for (const file of files) {
+        console.log(file.name)
+        const shapePath = join(shapeFolder, file.name)
+        const templateSource = removeComments(readFileSync(shapePath, 'utf8'))
+        const templateHashed: HashedFileContent = {
+          path: shapePath,
+          hashChunks: buildSimilarityHashmap(removeComments(templateSource)),
+          content: templateSource,
+        }
+        const similarity = estimateSimilarity(sourceHashed, templateHashed)
+        console.log(similarity)
+      }
+    }
+  })
+}
+
+function removeComments(source: string): string {
+  let result = ''
+  let isInSingleLineComment = false
+  let isInMultiLineComment = false
+
+  for (let i = 0; i < source.length; i++) {
+    if (isInSingleLineComment && source[i] === '\n') {
+      isInSingleLineComment = false
+      result += source[i] // Keep newline characters
+    } else if (
+      isInMultiLineComment &&
+      source[i] === '*' &&
+      source[i + 1] === '/'
+    ) {
+      isInMultiLineComment = false
+      i++ // Skip the '/'
+    } else if (
+      !isInMultiLineComment &&
+      source[i] === '/' &&
+      source[i + 1] === '/'
+    ) {
+      isInSingleLineComment = true
+      i++ // Skip the second '/'
+    } else if (
+      !isInSingleLineComment &&
+      source[i] === '/' &&
+      source[i + 1] === '*'
+    ) {
+      isInMultiLineComment = true
+      i++ // Skip the '*'
+    } else if (!isInSingleLineComment && !isInMultiLineComment) {
+      result += source[i]
+    }
+  }
+
+  return result
+}
+
+function buildSimilarityHashmap(input: string): HashedChunks[] {
+  const lines = splitLineKeepingNewlines(input)
+
+  const stringChunks = lines.flatMap((line) => {
+    const result = []
+    for (let i = 0; i < line.length; i += 64) {
+      result.push(line.slice(i, i + 64))
+    }
+    return result
+  })
+
+  checkIfLineCountIsCorrect(input, lines)
+
+  const map = new Map<string, number>()
+
+  for (const stringChunk of stringChunks) {
+    const element = map.get(stringChunk)
+
+    if (element !== undefined) {
+      map.set(stringChunk, element + stringChunk.length)
+    } else {
+      map.set(stringChunk, stringChunk.length)
+    }
+  }
+
+  // Transform that map to an array of Chunk objects
+  const chunks: HashedChunks[] = Array.from(map).map(([content, length]) => ({
+    content,
+    length,
+  }))
+  // Sort alphabetically by content
+  chunks.sort((lhs, rhs) => lhs.content.localeCompare(rhs.content))
+
+  return chunks
+}
+
+function splitLineKeepingNewlines(input: string): string[] {
+  const lines = []
+  let start = 0
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === '\n') {
+      const part = input.substring(start, i + 1)
+      lines.push(part)
+      start = i + 1
+    }
+  }
+
+  if (start < input.length) {
+    lines.push(input.substring(start))
+  }
+
+  return lines
+}
+
+function checkIfLineCountIsCorrect(input: string, lines: string[]): void {
+  const inputLines = input.split('\n')
+  if (inputLines.at(-1) === '') {
+    inputLines.pop()
+  }
+  const inputLineCount = inputLines.length
+  const linesLineCount = lines.length
+
+  if (inputLineCount !== linesLineCount) {
+    throw new Error(
+      `Line count mismatch: ${inputLineCount} vs ${linesLineCount}`,
+    )
+  }
+}
+
+export function estimateSimilarity(
+  lhs: HashedFileContent,
+  rhs: HashedFileContent,
+): number {
+  let lhsIndex = 0
+  let rhsIndex = 0
+  let sourceCopied = 0
+
+  while (true) {
+    const lhsIsDone = lhsIndex === lhs.hashChunks.length
+    if (lhsIsDone) {
+      break
+    }
+
+    while (rhsIndex < rhs.hashChunks.length) {
+      if (
+        // @ts-expect-error safe assume index is correct
+        lhs.hashChunks[lhsIndex].content <= rhs.hashChunks[rhsIndex].content
+      ) {
+        break
+      }
+
+      rhsIndex++
+    }
+
+    // @ts-expect-error safe assume index is correct
+    const lhsCount = lhs.hashChunks[lhsIndex].length
+    let rhsCount = 0
+
+    if (
+      rhsIndex < rhs.hashChunks.length &&
+      // @ts-expect-error safe assume index is correct
+      lhs.hashChunks[lhsIndex].content === rhs.hashChunks[rhsIndex].content
+    ) {
+      // @ts-expect-error safe assume index is correct
+      rhsCount = rhs.hashChunks[rhsIndex].length
+      rhsIndex++
+    }
+
+    sourceCopied += Math.min(lhsCount, rhsCount)
+    lhsIndex++
+  }
+
+  return sourceCopied / Math.max(lhs.content.length, rhs.content.length)
 }
