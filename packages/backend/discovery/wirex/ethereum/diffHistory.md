4e5d321b8e7d48cb566b9f52d22851b450cb4cd9
sekuba <29250140+sekuba@users.noreply.github.com>
2024-07-16 16:07:10 +0200

    Add Wirex Pay Chain (full, rpc missing)

---
+++ packages/backend/discovery/wirex/ethereum/diffHistory.md
@@ -0,0 +1,36 @@
+Generated with discovered.json: 0x384652373d3955e0ca3a072c0d345421fd5f38b8
+
+# Diff at Tue, 16 Jul 2024 14:02:20 GMT:
+
+- author: sekuba (<29250140+sekuba@users.noreply.github.com>)
+- current block number: 20319541
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract FflonkVerifier (0x0775e11309d75aA6b0967917fB0213C5673eDf81)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0x2B966b9824c11f274aa39f2f72cF25C4b47c3A78)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract PolygonDataCommittee (0x755e9A5B4BAEFc78Bb82BA7E6d2386CCB2F238a5)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract WirexValidiumPolygonCDK (0x78253E2E6120164bd826668A4C96Db20f78A94c9)
+    +++ description: None
+```
--- packages/config/src/projects/layer2s/wirex.ts
+++ packages/config/src/projects/layer2s/wirex.ts
@@ -1,15 +1,80 @@
-import { underReviewL2 } from './templates/underReview'
+import { NEW_CRYPTOGRAPHY, RISK_VIEW } from '../../common'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { Badge } from '../badges'
+import { polygonCDKStack } from './templates/polygonCDKStack'
 import { Layer2 } from './types'
 
-export const wirex: Layer2 = underReviewL2({
-  id: 'wirex',
+const discovery = new ProjectDiscovery('wirex')
+
+const membersCountDAC = discovery.getContractValue<number>(
+  'PolygonDataCommittee',
+  'getAmountOfMembers',
+)
+
+const requiredSignaturesDAC = discovery.getContractValue<number>(
+  'PolygonDataCommittee',
+  'requiredAmountOfSignatures',
+)
+
+const isForcedBatchDisallowed =
+  discovery.getContractValue<string>(
+    'WirexValidiumPolygonCDK',
+    'forceBatchAddress',
+  ) !== '0x0000000000000000000000000000000000000000'
+
+const upgradeability = {
+  upgradableBy: ['ProxyAdminOwner'],
+  upgradeDelay: 'No delay',
+}
+
+export const wirex: Layer2 = polygonCDKStack({
+  discovery,
+  badges: [Badge.DA.DAC],
+  daProvider: {
+    name: 'DAC',
+    bridge: {
+      type: 'DAC Members',
+      requiredSignatures: requiredSignaturesDAC,
+      membersCount: membersCountDAC,
+    },
+    riskView: {
+      ...RISK_VIEW.DATA_EXTERNAL_DAC({
+        membersCount: membersCountDAC,
+        requiredSignatures: requiredSignaturesDAC,
+      }),
+      sources: [
+        {
+          contract: 'PolygonDataCommittee',
+          references: [
+            'https://etherscan.io/address/0xace9269eac3419937093154dea0ad44c36df6963#code',
+          ],
+        },
+      ],
+    },
+    technology: {
+      name: 'Data is not stored on chain',
+      description:
+        'The transaction data is not recorded on the Ethereum main chain. Transaction data is stored off-chain and only the hashes are posted on-chain by the Sequencer, after being signed by the DAC members.',
+      risks: [
+        {
+          category: 'Funds can be lost if',
+          text: 'the external data becomes unavailable.',
+          isCritical: true,
+        },
+      ],
+      references: [
+        {
+          text: 'PolygonValidiumStorageMigration.sol - Etherscan source code, sequenceBatchesValidium() function',
+          href: 'https://etherscan.io/address/0x10d296e8add0535be71639e5d1d1c30ae1c6bd4c#code#F1#L126',
+        },
+      ],
+    },
+  },
   display: {
     name: 'Pay Chain',
     slug: 'wirex',
-    category: 'Validium',
-    provider: 'Polygon',
     description:
-      'Pay Chain is a Validium built on the Polygon CDK stack. It will be used as the infrastructure for the Wirex non-custodial debit cards.',
+      'Pay Chain is a Validium built on the Polygon CDK stack. It is used as payment chain for the Wirex non-custodial debit cards.',
     purposes: ['Payments'],
     links: {
       websites: ['https://wirexpaychain.com/'],
@@ -24,4 +89,31 @@ export const wirex: Layer2 = underReviewL2({
       ],
     },
   },
+  rpcUrl: '',
+  nonTemplateEscrows: [],
+  milestones: [
+    // {
+    //   name: 'Witness Chain Mainnet Launch',
+    //   link: 'https://x.com/witnesschain/status/1808153753897652256',
+    //   date: '2024-07-02',
+    //   description:
+    //     'L2 Diligence proofs are now posted to Witness Chain Mainnet by Eigenlayer operators.',
+    // },
+  ],
+  knowledgeNuggets: [],
+  rollupModuleContract: discovery.getContract('WirexValidiumPolygonCDK'),
+  rollupVerifierContract: discovery.getContract('FflonkVerifier'),
+  isForcedBatchDisallowed,
+  nonTemplateTechnology: {
+    newCryptography: {
+      ...NEW_CRYPTOGRAPHY.ZK_BOTH,
+    },
+  },
+  nonTemplateContracts: [
+    discovery.getContractDetails('PolygonDataCommittee', {
+      description:
+        'Validium data availability committee contract that allows the admin to setup the members of the committee and stores the required amount of signatures threshold.',
+      ...upgradeability,
+    }),
+  ],
 })
