5649589d944d10a16671a1f62298a81532030ea4
sekuba <29250140+sekuba@users.noreply.github.com>
2024-07-16 14:46:34 +0200

    Review GPT Protocol

---
+++ packages/backend/discovery/gpt/ethereum/diffHistory.md
@@ -0,0 +1,42 @@
+Generated with discovered.json: 0xf0a45e128e4f2888e4cba5c32451f4b0f293712a
+
+# Diff at Tue, 16 Jul 2024 12:44:15 GMT:
+
+- author: sekuba (<29250140+sekuba@users.noreply.github.com>)
+- current block number: 20319151
+
+## Description
+
+Initial discovery: PolygonCDK Validium (type 4) that shares its consensus implementation with the other PolygonCDK validiums deployed so far.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract FflonkVerifier (0x0775e11309d75aA6b0967917fB0213C5673eDf81)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract PolygonDataCommittee (0x75E26A2996DEAbA20386B6f3c1C957eFadb3f6E8)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0xada59D145126A746976F0F56477aafFEB3acc8e3)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract GptValidiumPolygonCDK (0xC4E903D3Af4c3d2e437492d602adcC9d9b536858)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract GPTProtocol (0xCdb4A8742ed7D0259b51E3454C46C9D6C48d5e88)
+    +++ description: None
+```
--- packages/config/src/projects/layer2s/gpt.ts
+++ packages/config/src/projects/layer2s/gpt.ts
@@ -1,13 +1,78 @@
-import { underReviewL2 } from './templates/underReview'
+import { NEW_CRYPTOGRAPHY, RISK_VIEW } from '../../common'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { Badge } from '../badges'
+import { polygonCDKStack } from './templates/polygonCDKStack'
 import { Layer2 } from './types'
 
-export const gpt: Layer2 = underReviewL2({
-  id: 'gpt',
+const discovery = new ProjectDiscovery('gpt')
+
+const membersCountDAC = discovery.getContractValue<number>(
+  'PolygonDataCommittee',
+  'getAmountOfMembers',
+)
+
+const requiredSignaturesDAC = discovery.getContractValue<number>(
+  'PolygonDataCommittee',
+  'requiredAmountOfSignatures',
+)
+
+const isForcedBatchDisallowed = //??
+  discovery.getContractValue<string>(
+    'GptValidiumPolygonCDK',
+    'forceBatchAddress',
+  ) !== '0x0000000000000000000000000000000000000000'
+
+const upgradeability = {
+  upgradableBy: ['ProxyAdminOwner'],
+  upgradeDelay: 'No delay',
+}
+
+export const gpt: Layer2 = polygonCDKStack({
+  discovery,
+  badges: [Badge.DA.DAC],
+  daProvider: {
+    name: 'DAC',
+    bridge: {
+      type: 'DAC Members',
+      requiredSignatures: requiredSignaturesDAC,
+      membersCount: membersCountDAC,
+    },
+    riskView: {
+      ...RISK_VIEW.DATA_EXTERNAL_DAC({
+        membersCount: membersCountDAC,
+        requiredSignatures: requiredSignaturesDAC,
+      }),
+      sources: [
+        {
+          contract: 'PolygonDataCommittee',
+          references: [
+            'https://etherscan.io/address/0xa36afb6b79a3d164a3d12c141c916becc6e012d8#code',
+          ],
+        },
+      ],
+    },
+    technology: {
+      name: 'Data is not stored on chain',
+      description:
+        'The transaction data is not recorded on the Ethereum main chain. Transaction data is stored off-chain and only the hashes are posted on-chain by the Sequencer, after being signed by the DAC members.',
+      risks: [
+        {
+          category: 'Funds can be lost if',
+          text: 'the external data becomes unavailable.',
+          isCritical: true,
+        },
+      ],
+      references: [
+        {
+          text: 'PolygonValidiumStorageMigration.sol - Etherscan source code, sequenceBatchesValidium() function',
+          href: 'https://etherscan.io/address/0x10d296e8add0535be71639e5d1d1c30ae1c6bd4c#code#F1#L126',
+        },
+      ],
+    },
+  },
   display: {
     name: 'GPT Protocol',
     slug: 'gpt',
-    category: 'Validium',
-    provider: 'Polygon',
     description:
       'GPT Protocol is a Validium built on the Polygon CDK stack. The purpose of the project is to create a decentralized market of AI compute power.',
     purposes: ['AI'],
@@ -30,12 +95,30 @@ export const gpt: Layer2 = underReviewL2({
     },
     activityDataSource: 'Blockchain RPC',
   },
-  // rpcUrl: 'https://sequencer.gptprotocol.io/',
-  transactionApi: {
-    type: 'rpc',
-    startBlock: 1,
-    defaultUrl: 'https://sequencer.gptprotocol.io/',
-    defaultCallsPerMinute: 1500,
+  nonTemplateEscrows: [],
+  milestones: [
+    // {
+    //   name: 'GPT Protocol Launch',
+    //   link: '',
+    //   date: '2024--',
+    //   description: 'GPT Protocol is now accessible to everyone.',
+    // },
+  ],
+  knowledgeNuggets: [],
+  rollupModuleContract: discovery.getContract('GptValidiumPolygonCDK'),
+  rollupVerifierContract: discovery.getContract('FflonkVerifier'),
+  rpcUrl: 'https://sequencer.gptprotocol.io/',
+  isForcedBatchDisallowed,
+  nonTemplateTechnology: {
+    newCryptography: {
+      ...NEW_CRYPTOGRAPHY.ZK_BOTH,
+    },
   },
-  escrows: [], // shared
+  nonTemplateContracts: [
+    discovery.getContractDetails('PolygonDataCommittee', {
+      description:
+        'Validium data availability committee contract that allows the admin to setup the members of the committee and stores the required amount of signatures threshold.',
+      ...upgradeability,
+    }),
+  ],
 })
